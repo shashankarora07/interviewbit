4c1bdaa23935f2b8b5e395ec6801ca7490900927
shashank <shashank.arora07@gmail.com>
2018-11-30 01:15:11 +0530

    max subarray added

--- subarray_max.c
+++ subarray_max.c
@@ -2,15 +2,50 @@
 #include <stdlib.h>
 
 int * no_of_subarray(int *, int);
+int * max_subarray(int *, int);
 int main()
 {
-	int arr[] = {1,2,-7,4,2,3};
+	int arr[] = {1,2,5,-7,2,3};
 
 	int len = sizeof(arr)/sizeof(arr[0]);	
 		
-	no_of_subarray(arr,len);
+	//no_of_subarray(arr,len);
+	max_subarray(arr,len);
 	return 0;
 }
+
+int * max_subarray(int *a, int len)
+{
+	int i, j, k = 0, len1 = 0, index = 0;
+	int *arr_out = (int *)malloc(sizeof(int)); 
+
+	
+	for (i = 0; i < len; i++) {
+        k = i;
+	//	printf("k = %d\n",k);
+		for (j = i; j < len; j++) {
+		//	printf("j = %d\n",j);
+			if (*(a + j) < 0) {
+				i = j;
+				break;
+			} else {
+				len1 = 1, index = 0;
+				while (k <= j) {
+					arr_out = (int *)realloc(arr_out,len1 * sizeof(int));
+					*(arr_out + index) = *(a + k);
+				//	printf("%d ",*(a+k));
+					printf("%d ",*(arr_out + index));
+					
+					k++, len1++, index++;
+				}
+				printf("\n");
+			}
+		   k = i;
+		}
+	}
+
+}
+
 int * no_of_subarray(int *a, int len)
 {
 	int i, j, k = 0;
