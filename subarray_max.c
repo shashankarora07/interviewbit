303cfe77ba1320182d4b15c077bfd68a983abffd
shashank <shashank.arora07@gmail.com>
2019-01-08 16:41:41 +0530

    add max_sum func

--- subarray_max.c
+++ subarray_max.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int * no_of_subarray(int *, int);
+int max_sum_contiguous_subarray(int *, int );
+
 int main()
 {
 	int arr[] = {1,2,-7,4,2,3};
@@ -11,20 +13,44 @@ int main()
 	no_of_subarray(arr,len);
 	return 0;
 }
+
 int * no_of_subarray(int *a, int len)
 {
-	int i, j, k = 0;
+	int i, j, k = 0, sum = 0;
+	int *max_arr = (int *)malloc(sizeof(int)*(len*(len+1)));
+	static int count = 0;
 	
 	for (i = 0; i < len; i++) {
 			k = i;
 		for (j = i; j < len; j++) {
 			while (k <= j) {
 				printf("%d ",*(a+k));
+				sum += *(a+k);
 				k++;
 			}
+			max_arr[count] = sum;
+			count++;
 			printf("\n");
 			k = i;
+			sum = 0;
 		}
 	}
+//	for (i = 0; i < count; i++)
+//		printf("%d\n",*(max_arr + i));
+	printf("count = %d\n",count);
+	max_sum_contiguous_subarray(max_arr, count);
+}
 
+int max_sum_contiguous_subarray(int *a, int len)
+{
+	int max_sum = *(a+0);
+
+	for (int i = 0; i < len; i++) {
+		if (*(a+i)>max_sum) {
+			max_sum = *(a+i);
+		}
+	}
+	
+	printf("max_sum in contiguous subarray = %d\n",max_sum);
+	return max_sum;
 }
