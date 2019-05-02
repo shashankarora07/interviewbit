8fddae31049ef54b7aefb9d7b96b287327fc4989
shashank <shashank.arora07@gmail.com>
2019-05-02 08:54:43 +0530

    update

--- reverse_word.c
+++ reverse_word.c
@@ -1,44 +1,62 @@
 #include<stdio.h>
 #include<string.h>
 
-char* reverseString(char *a)
+void reverseString(char *start, char *end)
 {
 	int i = 0;
-	int len = strlen(a);
-	printf("len = %d\n",len);
-	char c;
-	
-	for (i = 0; i < len/2; i++) {
-		c = *(a + len - i - 1);
-		*(a + len - i - 1) = *(a + i);
-		*(a + i) = c;
+	char temp;
+	while (start < end) {
+		temp = *start;
+		*start++ = *end;
+		*end-- = temp; 
 	}
-	
-	return a;
 }
 
 void reverseWord(char *s)
 {
-	
-	
-	char *start = s;
+		
+	char *word_start = s;
 	char *temp = s;
-	printf("%s\n",end);
-
+	while(*temp) {
+		temp++;
+		//printf("temp = %c and word_start = %c\n",*temp,*word_start);
+		if (*temp == '\0') {
+			reverseString(word_start, temp - 1);
+		}
+		else if (*temp == ' ') {
+			printf("temp = %c and word_start = %c\n",*temp,*word_start);
+			if (*word_start == *temp) {
+				//word_start = temp + 1;
+				printf("if same then temp = %c and word_start = %c\n",*temp,*word_start);
+				++word_start;
+			}
+			else {
+			printf("INSIDE Else :: temp = %c and word_start = %c\n",*temp,*word_start);
+			reverseString(word_start, temp - 1);
+			word_start = temp+1;
+			}
+		}
+	}
 	
+	printf("%s\n",s);
+	int len = strlen(s);
+	printf("len after reverse: %d\n",len);
 
-
+	reverseString(s, temp - 1);
+	printf("%s\n",s);
 
 }
 
 
 int main()
 {
-	char str[] = "I am Shashank Arora";
+	char str[] = "I am Shashank     Arora";
 	//"I ma knahsahS arorA" first reverse word and then reverse whole string
 	// OUTPUT: "Arora Shashank am I"
 	char *temp = str;
-
+	printf("actual string:%s\n",temp);
+	int len = strlen(temp);
+	printf("Actual len: %d\n",len);
 	reverseWord(temp);
 	
 	
