92a4d0667d4f4a283d6201118bf79eaa117acfdc
shashank <shashank.arora07@gmail.com>
2019-08-29 19:58:03 +0530

    common elements in two sorted array

---
+++ intersectionofTwoSortedArray.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Find the intersection of two sorted arrays.
+OR in other words,
+Given 2 sorted arrays, find all the elements which occur in both the arrays. */
+
+int intersectionTwoSortedArray(int *A, int lenA, int *B, int lenB)
+{
+	int i = 0, j = 0, k = 0;
+	int size = lenA + lenB;
+	int *out = (int *)malloc(sizeof(int) * size);
+
+	while(lenA && lenB)
+	{
+	}
+
+	PrintArray(out, size);
+}
+
+void PrintArray(int *a, int len)
+{
+	int i = 0;
+
+	for (i = 0; i < len; i++) {
+		printf("%d  ", a[i]);
+	}
+	printf("\n");
+
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		perror("Usage: size of array is not given");
+		exit(1);
+	}
+
+	int size = atoi(argv[1]);
+	//int arr[size];
+	
+	int A[] = {1,2,3,3,4,5,6};
+	int B[] = {3,3,5};
+
+	//for (i = 0; i < size; i++)
+	//	scanf("%d",&arr[size]);
+
+	int sizeA = sizeof(A)/sizeof(A[0]);
+	int sizeB = sizeof(B)/sizeof(B[0]);
+	intersectionTwoSortedArray(A,sizeA,B,sizeB);
+
+
+	return 0;
+}
