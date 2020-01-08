970f28c0ce9f462147d0a514f8bc524166a0656c
EE211366 <ee211366@pcz-ee211366.sasken.com>
2020-01-08 18:42:35 +0530

    bug is present. need to solve
    Signed-off-by: EE211366 <ee211366@pcz-ee211366.sasken.com>

--- rainWaterTrap.c
+++ rainWaterTrap.c
@@ -69,7 +69,8 @@ int rainWatertrap(int *a, int len)
 int main()
 {
 	//int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-	int arr[] = {3,0,0,2,0,4};
+	//int arr[] = {3,0,0,2,0,4};
+	int arr[] = {1,2,3,2,0,3,0,2,0,1,2,1};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	int unit = 0;
 
