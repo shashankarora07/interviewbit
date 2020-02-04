90b837fbd15d3093e88cdf39d35011cbdb0a33f9
EE211366 <ee211366@pcz-ee211366.sasken.com>
2020-02-04 20:08:18 +0530

    implemented new method to reverse bytes of integer
    Signed-off-by: EE211366 <ee211366@pcz-ee211366.sasken.com>

--- reverse_num.c
+++ reverse_num.c
@@ -53,6 +53,50 @@ unsigned int reverse_byte(unsigned int a)
 }
 
 
+unsigned int reverse_byteII(unsigned int a)
+{
+	int i;
+	unsigned int rev_byte = 0;
+	int SIZE = sizeof(int) * 8;
+	int mask = 0xFF;
+	int no = 0;
+	printf("no = %d and its binaryPrint\n", a);
+	binaryPrint(a);
+	for (i = 0; i < SIZE; i+=8) {
+			no = a & mask;
+			rev_byte |= (no << SIZE-i-8);
+			//binaryPrint(rev_byte);
+			a = a >> 8;
+		}
+
+	printf("rev byte = %d\n",rev_byte);
+	binaryPrint(rev_byte);
+}
+
+
+unsigned int reverse_byteIII(unsigned int a)
+{
+	int i;
+	unsigned int rev_byte = 0;
+	int SIZE = sizeof(a);
+	//int mask = 0xFF;
+	int no = a;
+	char temp = NULL;
+	
+	char *p = (char *)&no;
+
+	printf("no = %d and its binaryPrint\n", a);
+	binaryPrint(a);
+	for (i = 0; i < SIZE/2; i++) {
+			temp = *(p+i);
+			*(p+i) = *(p+SIZE-i-1);
+			*(p+SIZE-i-1) = temp;
+		}
+
+	printf("rev byte = %d\n",no);
+	binaryPrint(no);
+}
+
 int main()
 {
 
@@ -60,7 +104,9 @@ int main()
 	binaryPrint(A);
 	reverse(A);
 	A = 0x00005826;
-	reverse_byte(A);
+	//reverse_byte(A);
+	//reverse_byteII(A);
+	reverse_byteIII(A);
 
 
 	return 0;
