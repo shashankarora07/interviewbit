9c3d8fb4f73004e12cfafa14e3a9e263fbd8359e
EE211366 <ee211366@pcz-ee211366.sasken.com>
2020-02-19 19:49:13 +0530

    updated power of 4 using bitwise operator
    Signed-off-by: EE211366 <ee211366@pcz-ee211366.sasken.com>

--- mutipleAndPowerOF4.c
+++ mutipleAndPowerOF4.c
@@ -1,6 +1,37 @@
 #include<stdio.h>
 #include <stdlib.h>
 
+void powerOf4(int no)
+{
+	int cpyno = no;
+	while(no != 1) {
+		if (no % 4 != 0) {
+			printf("%d is not power of 4\n",cpyno);
+			return;
+		}
+		no /= 4; 
+	}
+	if (no == 1)
+		printf("%d is power of 4\n",cpyno);	
+}
+
+void powerOf4BitWise(int no)
+{
+	int cpyno = no;
+	if ( no && !(no & (no-1)) ) {
+		int countZeros = 0;
+		while (no > 1) {
+			no = no >> 1;
+			countZeros++;
+		}
+
+		if (!(countZeros & 1))
+			printf("%d is power of 4\n",cpyno);
+		else
+			printf("%d is not power of 4\n",cpyno);
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2) {
@@ -18,15 +49,8 @@ int main(int argc, char *argv[])
 	else
 		printf("%d is not mutiple of 4\n",no);
 
-	int cpyno = no;
-	while(no != 1) {
-		if (no % 4 != 0) {
-			printf("%d is not power of 4\n",cpyno);
-			return 0;
-		}
-		no /= 4; 
-	}
-	if (no == 1)
-		printf("%d is power of 4\n",cpyno);
+	powerOf4(no);
+	powerOf4BitWise(no);
 
+	return 0;
 }
