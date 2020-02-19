3a9803186c192a0cdedb9fac3bc209365182e895
shashank <shashank.arora07@gmail.com>
2020-02-19 09:08:32 +0530

    morse code

---
+++ morseCodeStringValidate.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+
+
+int morseCode_StringValidate(char *s[], int len)
+{
+	int i = 0, j = 0, dash = 0, dot = 0, count = 0;
+
+	for (i = 0; i < len; i++) {
+		int size = strlen(s[i]);
+		dash = 0, dot = 0, j = 0;
+		printf("size = %d\n",size);
+		while (j < size) {
+
+			while(s[i][j] == '.' && dot++ && ++j);
+			printf("dot = %d and j = %d\n",dot,j);
+
+			while(s[i][j] == '-' && dash++ && ++j);
+			printf("dash = %d and j = %d\n",dash,j);
+
+			/*if (s[i][j] == '.') {
+				dot++, j++;
+			}
+			if (s[i][j] == '-') {
+				dash++, j++;
+			}*/
+
+			//printf("dot = %d and dash = %d\n",dot,dash);
+
+		}
+		if ((dash == dot) && s[i][0] == '.' && s[i][size-1] == '-')
+			count++;
+
+	}
+
+	return count;
+}
+
+
+int main(int argc, char *argv[])
+{
+	/*if (argc != 2) {
+		perror("Usage: give string");
+		exit(0);
+	}	
+
+	char *str = argv[1];
+	*/
+	//char *str = "myname is shashankname arora namename  ";
+	//char *str = "   xDGBklKecz IAcOJYOH O  WY WPi     ";
+	//char *str = "d";
+	char *str[] = {"...----..--",".-.-.-.-"};
+	int len =2;
+	
+	int ret = -1;
+	printf("len = %d\n", len);
+	ret = morseCode_StringValidate(str,len);
+	printf("total no of messages coded correctly = %d\n",ret);
+
+	return 0;
+}
